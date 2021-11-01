e6f5838f02961a5221bc5a45291b3cbd25104f10
Ben Wylynko <17brtw@queensu.ca>
2021-10-31 21:28:05 -0400

    added dirs and scripts for payload code

---
+++ payload/image_capture/capture.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+def capture_img(format: str = 'png', res: tuple = (4144, 2822)):
+    """ 
+    Function to capture the pi camera image. 
+
+    params: 
+        format: File format in which to save the img
+        res: The pixel resolution of the image (height, width)
+    returns:
+        outpath: Path to the captured image
+    """ 
---
+++ payload/image_compression/compress.py
@@ -0,0 +1,13 @@
+
+
+
+def compress_img(img: Path, outsize: tuple = (2000, 1000)):
+    """ 
+    params: 
+        img: Path to the input image
+    returns:
+        outpath: Path to the compressed image (.jpeg)
+    """ 
+
+
+
