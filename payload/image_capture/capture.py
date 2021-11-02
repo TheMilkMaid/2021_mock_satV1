45ae46274408135669a2c840e4269f74c9027730
BenWylynko <47759901+BenWylynko@users.noreply.github.com>
2021-11-02 19:38:55 -0400

    Update capture.py

--- payload/image_capture/capture.py
+++ payload/image_capture/capture.py
@@ -4,13 +4,10 @@
 
 
 
-def capture_img(format: str = 'png', res: tuple = (4144, 2822)):
+def capture_img(imgpath):
     """ 
     Function to capture the pi camera image. 
 
     params: 
-        format: File format in which to save the img
-        res: The pixel resolution of the image (height, width)
-    returns:
-        outpath: Path to the captured image
+        imgpath: Path to location to store the image. 
     """ 
