ae0df3318ce40eb85f2db9a625c9ec1407126ad2
Ben Wylynko <68921224+TheMilkMaid@users.noreply.github.com>
2021-11-02 13:24:30 -0400

    Update compress.py

--- payload/image_compression/compress.py
+++ payload/image_compression/compress.py
@@ -1,12 +1,10 @@
 
 
 
-def compress_img(img: Path, outsize: tuple = (2000, 1000)):
+def compress_img(img: Path, outpath: Path):
     """ 
     params: 
         img: Path to the input image
-    returns:
-        outpath: Path to the compressed image (.jpeg)
     """ 
 
 
