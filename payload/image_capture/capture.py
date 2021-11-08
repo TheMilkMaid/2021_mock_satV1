87b61f0e5a3d865a48c4b1cd7347a11d7ed03aa1
Evan Rimer <33584254+evanrimer@users.noreply.github.com>
2021-11-08 15:35:26 -0500

    Update capture.py

--- payload/image_capture/capture.py
+++ payload/image_capture/capture.py
@@ -6,7 +6,15 @@
 
 def capture_img(imgpath):
     """ 
-    Function to capture the pi camera image. 
+    from picamera import PiCamera
+    from time import sleep
+    
+    camera = PiCamera()
+    camera.resolution = (2592,1944) # 4:3 aspect ratio
+    camera.start_preview()
+    sleep(3)
+    camera.capture(imgpath)
+    camera.stop_pewview()
 
     params: 
         imgpath: Path to location to store the image. 
