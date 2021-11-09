1f239de311590b78d8a6b97abf0007eea7873cbe
matthewuhthoff <mattuhthoff@gmail.com>
2021-11-09 12:28:35 -0500

    added function code

--- payload/image_compression/compress.py
+++ payload/image_compression/compress.py
@@ -1,7 +1,11 @@
+from PIL import Image
+import time
+
+
+def compress_img(img: Path, outpath: Path):
 
 
 
-def compress_img(img: Path):
     """ 
     params: 
         img: Path to the input image
@@ -9,5 +13,15 @@ def compress_img(img: Path):
         outpath: Path to the compressed image (.jpeg)
     """ 
 
+
+			
+
+
+	img1 = Image.open(img)
+	
+	time0 = time.time()
+	img1.save(outpath, quality=75)
+	compress_time = time.time() - time0
+	print(compress_time)
 
 
