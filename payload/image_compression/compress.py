32ac5a87f93f93119e2ebfcbacb7c66ad01f6e5d
Ben Wylynko <17brtw@queensu.ca>
2021-11-10 13:05:53 -0500

    modified capture, compression code

--- payload/image_compression/compress.py
+++ payload/image_compression/compress.py
@@ -2,26 +2,27 @@ from PIL import Image
 import time
 
 
-def compress_img(img: Path, outpath: Path):
-
-
+def compress_img(img, outpath):
 
     """ 
     params: 
-        img: Path to the input image
-    returns:
-        outpath: Path to the compressed image (.jpeg)
-    """ 
+        img: Path to the input image to compress (eg.'./image_capture/test_img.png')
+        outpath: Path to the directory to save the compressed image (eg.'./image_capture/test_img.png')
+        
+    """
+    
+    outfile = outpath + '/compressed.png'
+    imgObj = Image.open(img)
+    
+    time0 = time.time()
+    imgObj = imgObj.resize((1944, 1458), Image.ANTIALIAS)
+    imgObj.save(outfile, quality=75)
+    compress_time = time.time() - time0
+    print(f"Compressed the image in {compress_time}")
 
 
-			
-
-
-	img1 = Image.open(img)
-	
-	time0 = time.time()
-	img1.save(outpath, quality=75)
-	compress_time = time.time() - time0
-	print(compress_time)
+#for testing
+if __name__ == "__main__":
+    compress_img('../image_capture/test_img.png', '.')
 
 
